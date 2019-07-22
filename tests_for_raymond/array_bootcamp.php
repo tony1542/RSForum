47de9c349ddec53104def8a0b7bf3d14227d0804
Tony <tonyreed1542@gmail.com>
2019-07-21 20:31:36 -0500

    array exercises

---
+++ tests_for_raymond/array_bootcamp.php
@@ -0,0 +1,34 @@
+<?php
+
+// Sort the following array in alphabetical order using an array function (look up PHP array functions if you don't know it off-hand)
+$array_of_unsorted_words = [
+    'Shovel',
+    'Green',
+    'Car',
+    'Spatula',
+    'Watering can',
+    'Bandos'
+];
+
+// Fill the following array with 5 different weapons and their requirements
+// Make the level requirement the array key and the weapon name the value
+$array_of_weapons_and_requirements = [
+
+];
+
+// Print out the highest leveled player, then the lowest leveled player by accessing the array key directly
+// Something like: $array[$key]
+$array_of_total_levels = [
+    'Zezima'        => 2277,
+    'Noob_slayer13' => 1588,
+    'Mediocre_moo'  => 1182,
+    'Garbage_fire'  => 884
+];
+
+// Write a foreach loop to iterate over the following array and print out its contents
+$array_of_justice = [
+    'Tony',
+    'Raymond',
+    'Susan',
+    'Bob'
+];
