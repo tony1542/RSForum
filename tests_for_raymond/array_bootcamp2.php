a494c7c70245cd80817aac5033a19bfc8a996675
Tony <tonyreed1542@gmail.com>
2019-07-22 19:14:59 -0500

    glhf

---
+++ tests_for_raymond/array_bootcamp2.php
@@ -0,0 +1,29 @@
+<?php
+
+// Fill this array up with a random string with a length of 5, 10 times using a loop.
+$random_array = [];
+
+// Filter out all elements in this array that contain the letter 'e'.
+// Do it once manually, and then do it a second time using a built-in PHP function.
+$array_to_filter = [
+    'Basic_key',
+    'Advanced_key',
+    'Master_key',
+    'Dog'
+];
+
+// Combine the following 2 arrays once manually, and then once using a built-in PHP function.
+// Once they are combined, print out ONLY the values that are above 2000.
+$array_of_total_levels = [
+    'Zezima'        => 2277,
+    'Noob_slayer13' => 1588,
+    'Mediocre_moo'  => 1182,
+    'Garbage_fire'  => 884
+];
+
+$array_of_total_levels2 = [
+    'player82' => 481,
+    'B0aty'    => 2276,
+    'Schlitz'  => 2270,
+    'tony_x'   => 1111
+];
