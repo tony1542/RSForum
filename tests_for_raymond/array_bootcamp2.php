3baebc507e79fe1307d58d7fded7353e91d212b4
kizekaze <rae337@hotmail.com>
2019-07-25 17:53:01 -0500

    Finished all exercises, google was heavily used. memory is lacking

--- tests_for_raymond/array_bootcamp2.php
+++ tests_for_raymond/array_bootcamp2.php
@@ -3,6 +3,40 @@
 // Fill this array up with a random string with a length of 5, 10 times using a loop.
 $random_array = [];
 
+function genRanStr($length = 5) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLength = strLen($characters);
+    $ranString = '';
+    for ($i = 0; $i < $length; $i++){
+        $ranString .= $characters[rand(0, $charLength - 1)];
+    }
+    return $ranString;
+
+}
+
+//echo genRanStr();
+
+for ($i = 0; $i < 10; $i++) {
+    $random_array[] = genRanStr();
+
+}
+
+print_r($random_array);
+
+
+//this took SOOOO LOOOONGGGG OMG I FINALLY GOT IT HOLY FUCK
+
+
+
+
+
+
+
+
+
+
+
+
 // Filter out all elements in this array that contain the letter 'e'.
 // Do it once manually, and then do it a second time using a built-in PHP function.
 $array_to_filter = [
@@ -12,8 +46,36 @@ $array_to_filter = [
     'Dog'
 ];
 
+
+
+//This is my manual attempt but I still use the array_filter, so I guess it isn't manual.
+
+function leFilter($string){
+
+    return strpos($string, 'e') === false;
+}
+$newArray_to_filter = array_filter($array_to_filter, 'leFilter');
+print_r($newArray_to_filter);
+
+
+
+
+
+
+
+
+//PHP Function
+print_r(array_filter($array_to_filter, function ($var) { return (stripos($var, 'e') === false); }));
+
+
+
+
+
+
 // Combine the following 2 arrays once manually, and then once using a built-in PHP function.
 // Once they are combined, print out ONLY the values that are above 2000.
+
+
 $array_of_total_levels = [
     'Zezima'        => 2277,
     'Noob_slayer13' => 1588,
@@ -27,3 +89,41 @@ $array_of_total_levels2 = [
     'Schlitz'  => 2270,
     'tony_x'   => 1111
 ];
+//Manual merging, son.
+$StonkMains = $array_of_total_levels + $array_of_total_levels2;
+
+
+function filterArray($value){
+    return ($value > 2000);
+}
+
+$filteredArray = array_filter($StonkMains, 'filterArray');
+
+foreach($filteredArray as $k => $v){
+    echo " $k = $v, ";
+}
+
+
+
+
+
+
+//Built-in PHP Function merging, son.
+$StackedMain = array_merge($array_of_total_levels,$array_of_total_levels2);
+
+function maxAcc($max_values)
+{
+    if ($max_values > 2000)
+    {
+        return true;
+    }
+    return false;
+}
+print_r(array_filter($StackedMain, "maxAcc"));
+
+
+
+
+?>
+
+
