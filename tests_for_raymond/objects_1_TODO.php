02324ebfe6643fabb975ed35f953ecd3fb2c72e7
Tony <tonyreed1542@gmail.com>
2019-08-04 16:49:27 -0500

    add todos

---
+++ tests_for_raymond/objects_1_TODO.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * What I want to see:
+ *
+ * 1. Make a class called 'runescape_player'
+ * 2. Make it have a constructor that accepts a number for that player's total level
+ * 3. Assign the class a total_level variable that the constructor uses, like in the instructions script
+ * 4. Print out that total level by either accessing it as a variable, or via a function. Again like in the instructions script
+ */
