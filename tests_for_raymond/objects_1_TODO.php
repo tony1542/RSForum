71baac2bc7cb2f59265a84e099297c389be3d415
KizeKaze <53065440+KizeKaze@users.noreply.github.com>
2019-08-04 18:36:26 -0500

    Removed extra space to make it easier to read

--- tests_for_raymond/objects_1_TODO.php
+++ tests_for_raymond/objects_1_TODO.php
@@ -9,21 +9,16 @@
  * 4. Print out that total level by either accessing it as a variable, or via a function. Again like in the instructions script
  */
 
-
-
    class runescape_player {
 
        public $TL = '';
-
-
+      
         public function __construct($TL){
 
            $this->totalLevel = $TL;
 
      }
-
    }
-
 
    class max_club{
 
@@ -33,24 +28,14 @@
 
            $this->maxExp = $MS;
 
-
        }
-
-
 }
-
-
-
-
     # I was going to have 1 construct but I learned I can't have 2 in 1 class, or at least not that someone of my skill level knows of.
 
     $player = new runescape_player('2277');
     $exp = new max_club('451,380,905');
+
    echo "Total Level for Tony_X is " .$player->totalLevel. " and his total EXP is " . $exp->maxExp . " for now...";
-
-
-
-
 
    //Also doing it via your 2nd method just to say I did it.
 
@@ -65,27 +50,9 @@
         public function getTotalLVL() {
             return $this->totalLevel;
         }
-
     }
 
     $player2 = new runescape_player2('9999');
+
     echo "BREAKING NEWS: KIZE HAS HIT THE MAX RECORD OF " .$player2->getTotalLVL() . " TOTAL LEVEL, RUNESCAPE IS CROWNING HIM THE BEST EVER AT RUNESCAPE WITH 16 KING BLACK DRAGON KILLS!!";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
