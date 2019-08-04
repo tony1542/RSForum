9cc11e4b31e99da6037606f4cad1f9ccfea10921
kizekaze <rae337@hotmail.com>
2019-08-04 17:58:03 -0500

    Added Manual way to filter an array how you wanted it, gave up my soul to do so

--- tests_for_raymond/objects_1_TODO.php
+++ tests_for_raymond/objects_1_TODO.php
@@ -8,3 +8,84 @@
  * 3. Assign the class a total_level variable that the constructor uses, like in the instructions script
  * 4. Print out that total level by either accessing it as a variable, or via a function. Again like in the instructions script
  */
+
+
+
+   class runescape_player {
+
+       public $TL = '';
+
+
+        public function __construct($TL){
+
+           $this->totalLevel = $TL;
+
+     }
+
+   }
+
+
+   class max_club{
+
+       public $MS = '';
+
+       public function __construct($MS){
+
+           $this->maxExp = $MS;
+
+
+       }
+
+
+}
+
+
+
+
+    # I was going to have 1 construct but I learned I can't have 2 in 1 class, or at least not that someone of my skill level knows of.
+
+    $player = new runescape_player('2277');
+    $exp = new max_club('451,380,905');
+   echo "Total Level for Tony_X is " .$player->totalLevel. " and his total EXP is " . $exp->maxExp . " for now...";
+
+
+
+
+
+   //Also doing it via your 2nd method just to say I did it.
+
+    class runescape_player2 {
+
+        protected $TL = '';
+
+        public function __construct($TL){
+            $this->totalLevel = $TL;
+        }
+
+        public function getTotalLVL() {
+            return $this->totalLevel;
+        }
+
+    }
+
+    $player2 = new runescape_player2('9999');
+    echo "BREAKING NEWS: KIZE HAS HIT THE MAX RECORD OF " .$player2->getTotalLVL() . " TOTAL LEVEL, RUNESCAPE IS CROWNING HIM THE BEST EVER AT RUNESCAPE WITH 16 KING BLACK DRAGON KILLS!!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
