e42a28a63c8dbece1118069254eb674b6f1c9529
Tony Reed <tonyreed1542@gmail.com>
2019-08-20 21:21:14 -0500

    test

--- tests_for_raymond/car.php
+++ tests_for_raymond/car.php
@@ -3,60 +3,53 @@
 // This example is just going to build off of the last example
 // More variables, more functions, a little more in-depth
 class rune_char {
-
     public $gear = ''; //gear is Make
     public $str = ''; //Str is year
     public $def = ''; // def is miles
     public $user = '';
     public $gp = 0; // Should be by itself
-    public $kizeGP = 1000000;
-    public $tonyGP = 999999999;
-
-    //public function __construct($gear, $str, $def, $user, $kizeGP, $tonyGP) {
+    
     public function __construct($gear, $str, $def, $user, $gp) {
-
         $this->gear = $gear;
         $this->str = $str;
         $this->def = $def;
         $this->user = $user;
-       $this->kizeGP = $kizeGP;
-       $this->tonyGP = $tonyGP;
+        $this->gp = $gp;
     }
-        //Calculation to check item and if user can afford it.
-        public function print_item(){
-            //if you were checking for a word and didn't want uppper/lower to matter, use strtolower($this->$att);
-            $gear = strtolower($this->gear);
-            $user = $this->user;
-            $itemCost = $this->itemCost;
-            $gp = null;
-            $can_buy = null;
-
-            if($gear === 'bandos') { // I know the && $user === Tony X isn't needed, just kept it. only 1 user so I dont need to be so specific.
-                $gp = 49152369;
-                $can_buy ='✓';
-            }
-            if ($gear === 'bronze') {
-                $gp = 1266;
-                $can_buy ="✓, but it's beneath him.";
-            }
-            if ($gear === 'justiciar'){
-                $gp = 67490862;
-                $can_buy = '✓';
-            }
-            echo "<h5>Gp Cost: $" . number_format($gp, 2 ,'.', ',') . "</h5>";
-                echo "<h5>Can Tony X Buy this?: " . $can_buy . "</h5>";
+    
+    //Calculation to check item and if user can afford it.
+    public function print_item() {
+        //if you were checking for a word and didn't want uppper/lower to matter, use strtolower($this->$att);
+        $gear = strtolower($this->gear);
+        $gp = null;
+        $can_buy = null;
+        
+        // check for $gp here in the if statement instead of re-assinging it on line 29 right?
+        // something similar to this:
+        // if ($gear === 'bandos' && $gp >= 49152369
+        if ($gear === 'bandos') { // I know the && $user === Tony X isn't needed, just kept it. only 1 user so I dont need to be so specific.
+            $gp = 49152369;
+            $can_buy = true;
         }
-        //helper function that I'll use for Tony so his eyes stop bleeding.
-        public function help_tony_print_data(){
-            echo "<h5>Gear : " . $this->gear . "</h5>";
-            echo "<h5>Strength: " . $this->str . "</h5>";
-            echo "<h5>Defence: " . $this->def . "</h5>";
+        
+        if ($gear === 'bronze') {
+            $gp = 1266;
+            $can_buy = true;
         }
-        public function can_Kize_buy(){
-
-
-
-
+        
+        if ($gear === 'justiciar') {
+            $gp = 67490862;
+            $can_buy = true;
         }
-
+        
+        if ($can_buy) {
+            echo "fuck yeeeeaaaaahhhhh boiiiiiii";
+        }
+    }
+    
+    public function print_data() {
+        echo "<h5>Gear : " . $this->gear . "</h5>";
+        echo "<h5>Strength: " . $this->str . "</h5>";
+        echo "<h5>Defence: " . $this->def . "</h5>";
+    }
 }
--- tests_for_raymond/object_2_script.php
+++ tests_for_raymond/object_2_script.php
@@ -2,46 +2,6 @@
 
 // including our class so we can use it in this script
 include 'car.php';
-include 'rune_Char.php';
-
-$bandos = new rune_char('Bandos', 0, 65, 'Tony X');
-$bandos->print_item();
-$bandos->help_tony_print_data();
-
-echo "<hr>";
-
-$bronze= new rune_char('Bronze', 99, 1, 'Tony X');
-$bronze->print_item();
-$bronze->help_tony_print_data();
-
-echo "<hr>";
-
-$Justiciar = new rune_Char('Justiciar', 643, 99999999, 'Tony X');
-$Justiciar->print_item();
-$Justiciar->help_tony_print_data();
-
-echo "<hr>";
-
-$dragon = new runescape_user('Dragon', 0, 60, 'Kize');
-$dragon->print_item2();
-$dragon->help_kize_print_data();
-
-echo "<hr>";
-
-$iron = new runescape_user('Iron', 0, 10, 'Kize');
-$iron->print_item2();
-$iron->help_kize_print_data();
-
-echo "<hr>";
-
-$bronzet = new runescape_user('bronzeT', 19999901, 1, "Kize");
-$bronzet->print_item2();
-$bronzet->help_kize_print_data();
-
-if ($bandos->user === "Tony X" && $dragon->user2 === 'Kize') {
-    echo "<hr>";
-    echo "While " . $bandos->user . " can afford bandos, " . $dragon->user2 . " cannot afford Bandos. GG";
-}
 
 // new up two instances of the class, one for me, one for you to compare
 $bronze = new rune_char('Bronze', 99, 1, 'Tony X', 99999999);
