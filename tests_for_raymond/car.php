49cdd4596cc89d1a73b8449a880378776f9f2914
kizekaze <rae337@hotmail.com>
2019-08-21 15:52:33 -0500

    Latest and greatest update from the Kize.

--- tests_for_raymond/car.php
+++ tests_for_raymond/car.php
@@ -21,29 +21,28 @@ class rune_char {
     public function print_item() {
         //if you were checking for a word and didn't want uppper/lower to matter, use strtolower($this->$att);
         $gear = strtolower($this->gear);
-        $gp = null;
-        $can_buy = null;
+        $gp = $this->gp;
+        $can_buy = false; //set can_buy to false so it will auto echo get rinsed until you pass enough gp into the if statement.
         
         // check for $gp here in the if statement instead of re-assinging it on line 29 right?
         // something similar to this:
         // if ($gear === 'bandos' && $gp >= 49152369)
-        if ($gear === 'bandos') { // I know the && $user === Tony X isn't needed, just kept it. only 1 user so I dont need to be so specific.
-            $gp = 49152369;
+        if ($gear === 'bandos' && $gp >= 49152369) {
             $can_buy = true;
         }
         
-        if ($gear === 'bronze') {
-            $gp = 1266;
+        if ($gear === 'bronze' && $gp >= 1266) {
             $can_buy = true;
         }
         
-        if ($gear === 'justiciar') {
-            $gp = 67490862;
+        if ($gear === 'justiciar' && $gp >= 67490862) {
             $can_buy = true;
         }
         
-        if ($can_buy) {
-            echo "fuck yeeeeaaaaahhhhh boiiiiiii";
+        if ($can_buy == true) {
+            echo $this->user . " can buy this gear.";
+        } else {
+            echo  $this->user ." can not buy this gear, get rinsed kid; you're not tall enough to ride.";
         }
     }
     
--- tests_for_raymond/object_2_script.php
+++ tests_for_raymond/object_2_script.php
@@ -4,9 +4,29 @@
 include 'car.php';
 
 // new up two instances of the class, one for me, one for you to compare
-$bronze = new rune_char('Bronze', 99, 1, 'Tony X', 99999999);
-$rune = new rune_char('Rune', 25, 85, 'KizeKaze', 11111111);
+$Runescape_playerTony = new rune_char('bandos', 99, 60, 'Tony X', 99999999);
+$Runescape_playerTony->print_item();
+$Runescape_playerTony->print_data();
+echo "<hr>";
+$Runescape_playerZez = new rune_char('bronze', 27, 1, 'Zezima', 1);
+$Runescape_playerZez->print_item();
+$Runescape_playerZez->print_data();
 
-if ($bronze->user === 'Tony X' && $rune->user === 'KizeKaze') {
-    // do things
+echo "<hr>";
+
+$Runescape_playerKize = new rune_char('justiciar', 75, 85, 'KizeKaze', 3000000);
+$Runescape_playerKize->print_item();
+$Runescape_playerKize->print_data();
+
+echo "<hr>";
+
+$Runescape_playerShadeGang = new rune_char('bandos', 69, 35, 'ShadeGang', 999999999);
+$Runescape_playerShadeGang->print_item();
+$Runescape_playerShadeGang->print_data();
+
+echo "<hr>";
+
+
+if ($Runescape_playerTony->user === 'Tony X' && $Runescape_playerKize->user === 'KizeKaze') {
+    echo $Runescape_playerTony->user . " can afford Bandos while " . $Runescape_playerKize->user . " can't afford Justiciar!";
 }
