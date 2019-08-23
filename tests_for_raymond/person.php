092ba992461c4b275e3d42a2c4d92f267e7c5400
Tony Reed <tonyreed1542@gmail.com>
2019-08-23 12:37:37 -0500

    add example class with todos

---
+++ tests_for_raymond/person.php
@@ -0,0 +1,15 @@
+<?php
+
+class person {
+    public $age = null;
+    
+    public function __construct($age) {
+        // TODO set the properties $name & $age to our parameters sent in
+    }
+    
+    public function get_status() {
+        // TODO if the age is below 25, print out 'young'
+        // if the age is between 25 and 35 print out 'adult'
+        // if the age is above 35, print 'old'
+    }
+}
