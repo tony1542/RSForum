5cd3060684411aa72b381c58c13ac190161e43aa
Tony Reed <tonyreed1542@gmail.com>
2019-08-23 12:37:20 -0500

    add instr

---
+++ tests_for_raymond/object_3_script.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'person.php';
+
+// TODO make 3 different instances of the 'person' class and pass it the 3 ages shown in the 'get_status' function
+// have it print out the 3 different strings shown in the function's TODO
