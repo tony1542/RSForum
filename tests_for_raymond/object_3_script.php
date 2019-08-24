b8edfde37cdb1f0d11396b9ba2254a70c3c92b8a
kizekaze <rae337@hotmail.com>
2019-08-24 18:55:05 -0500

    Added functions in TODO

--- tests_for_raymond/object_3_script.php
+++ tests_for_raymond/object_3_script.php
@@ -4,3 +4,24 @@ include 'person.php';
 
 // TODO make 3 different instances of the 'person' class and pass it the 3 ages shown in the 'get_status' function
 // have it print out the 3 different strings shown in the function's TODO
+$Tony = new person('23', 'Tony');
+$Tony->get_status();
+$Tony->print_data();
+
+echo "<hr>";
+
+$Ray = new person('26', "Ray");
+$Ray->get_status();
+$Ray->print_data();
+
+echo "<hr>";
+
+$Susan = new person("52", "Susan");
+$Susan->get_status();
+$Susan->print_data();
+
+echo "<hr>";
+
+$Ghost = new person('1250', "Casper");
+$Ghost->get_status();
+$Ghost->print_data();
--- tests_for_raymond/person.php
+++ tests_for_raymond/person.php
@@ -2,14 +2,50 @@
 
 class person {
     public $age = null;
+    public $name = "";
     
-    public function __construct($age) {
+    public function __construct($age, $name) {
         // TODO set the properties $name & $age to our parameters sent in
+        $this->age = $age;
+        $this->name = $name;
     }
     
     public function get_status() {
         // TODO if the age is below 25, print out 'young'
         // if the age is between 25 and 35 print out 'adult'
         // if the age is above 35, print 'old'
+
+        $name = strtolower($this->name);
+        $age = $this->age;
+        $old = false;       //set to false until true
+        $adult = false;     //set to false until true
+        $young = false;     //set to false until true
+
+        //Checking age
+        if ($age > 35 && $age < 130){
+            $old = true;
+        } else if ($age <= 35 && $age >= 25){
+            $adult = true;
+        }else if ($age < 25 && $age > 0){
+            $young = true;
+        }
+
+        if ($old == true) {
+            echo  $name . " is old as hell";
+        }else if ($adult == true) {
+            echo  $name . " is extremely adult";
+        }else if ($young == true) {
+            echo  $name . " is a young baby, ready to be smothered with a pillow.";
+        } else {
+            echo $name .", this is all a dream; wake up!";
+        }
+
     }
+
+    public function print_data() {
+        echo "<h5>Name: " . $this->name ."</h5>";
+        echo "<h5>age: " . $this->age ."</h5>";
+    }
+
+
 }
