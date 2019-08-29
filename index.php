0842c81e095ee873f5b624d7dee2faede9ca72da
Tony Reed <tonyreed1542@gmail.com>
2019-08-29 16:54:13 -0500

    updates to project

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
-include 'classes/Skills.php';
-
-echo '<h1>Showing icons</h1>';
-foreach (Skills::ALL as $skill) {
-    echo '<img src="' . Skills::getSkillIconFromName($skill) . '" />';
-    echo '<br>';
-}
-
-echo '<h1>Invalid usage</h1>';
-Skills::getSkillIconFromName('Shovel');
+include 'vendor/autoload.php';
