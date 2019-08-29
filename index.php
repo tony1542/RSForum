c9109c362fca05259c2326de572ca2a00e712566
Tony Reed <tonyreed1542@gmail.com>
2019-08-29 17:07:27 -0500

    add todos

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-# Classes go into the /src directory; follow link below for more details (based upon our composer.json file)
+# Classes go into the /src/Classes directory; follow link below for more details (based upon our composer.json file)
 # @see https://getcomposer.org/doc/04-schema.md#psr-4
 
 # 1. Run 'php composer.phar install' to pull down our copy of the composer.json contents and have it run ok.
@@ -9,3 +9,6 @@
 # 4. Print out every single one of them within this script. Example, "Bronze: 1". So show the name of the class & what the defence level is that is required.
 
 include 'vendor/autoload.php';
+
+$bronze = new \App\Classes\Bronze();
+dump($bronze->getDefenceLevel());
