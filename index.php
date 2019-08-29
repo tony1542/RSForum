48993d6c21e37aef7f43e8058bf8242f3c94546b
Tony Reed <tonyreed1542@gmail.com>
2019-08-29 16:58:29 -0500

    add basic .json

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
+# Classes go into the /src directory; follow link below for more details (based upon our composer.json file)
+# @see https://getcomposer.org/doc/04-schema.md#psr-4
+
 include 'vendor/autoload.php';
