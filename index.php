62f5216cabb4455cdfeb06a6ae777bd03115adec
Tony Reed <tonyreed1542@gmail.com>
2019-08-29 17:03:12 -0500

    update index with instr

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
 # Classes go into the /src directory; follow link below for more details (based upon our composer.json file)
 # @see https://getcomposer.org/doc/04-schema.md#psr-4
 
+# 1. Run 'php composer.phar install' to pull down our copy of the composer.json contents and have it run ok.
+# 2. Fill out the methods within /src/Classes
+
 include 'vendor/autoload.php';
