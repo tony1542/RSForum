0c81d43e2c36927a866a8a284bb16bc1c1ce5c36
Tony Reed <tonyreed1542@gmail.com>
2019-09-19 20:45:41 -0500

    add todos in index

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+// TODO
+// Look at /public/views/home_page.php & complete todos
+// Look at /public/views/partials/header.php & complete todos
+
 include 'vendor/autoload.php';
 
 view('home_page');
