761f93f2e27bc987f62174a7adfe1abf52a61b25
Tony Reed <tonyreed1542@gmail.com>
2019-09-20 20:32:04 -0500

    add abstract parent controller

--- src/Controllers/AbstractBaseController.php
+++ src/Controllers/AbstractBaseController.php
@@ -4,17 +4,6 @@ namespace App\Controllers;
 
 abstract class AbstractBaseController
 {
-    abstract protected function includePrefix();
-    
-    /**
-     * Function's purpose is to get include path for using view()
-     * @see view()
-     */
-    protected function getIncludePrefix()
-    {
-        return $this->includePrefix();
-    }
-    
     /**
      * Checking if the current user has permissions for the requested action
      * (Letting the controller that is created determine this)
