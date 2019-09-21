5a413b965fabb75e7b19d3fb8dab925866b4c21c
Tony Reed <tonyreed1542@gmail.com>
2019-09-21 12:05:56 -0500

    WIP

--- public/views/partials/header.php
+++ public/views/partials/header.php
@@ -18,15 +18,15 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class='navbar-nav'>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sign-In</a>
+                    <a class="nav-link" href="/User/SignIn">Sign-In</a>
                 </li>
                 <li class='nav-item'>
-                    <a class="nav-link" href="#">Register</a>
+                    <a class="nav-link" href="/User/Register">Register</a>
                 </li>
             </ul>
         </div>
---
+++ src/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class UsersController extends AbstractBaseController
+{
+    public function canAccess($action, $parameters = []) {
+       return true;
+    }
+    
+    public function index() : void
+    {
+    
+    }
+    
+    public function register()
+    {
+        view('register');
+    }
+}
