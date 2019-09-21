c723982b0528803837f959d734ae699a57006fa2
Tony Reed <tonyreed1542@gmail.com>
2019-09-20 20:36:23 -0500

    rename

--- src/Controllers/PagesController.php
+++ src/Controllers/PagesController.php
@@ -3,12 +3,13 @@
 namespace App\Controllers;
 
 class PagesController extends AbstractBaseController {
-    public function index()
+    public function canAccess($action, $parameters = [])
     {
-        view('home_page');
+        return true;
     }
     
-    public function canAccess($action, $parameters = []) {
-        return true;
+    public function index() : void
+    {
+        view('home_page');
     }
 }
