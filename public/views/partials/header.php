78672ac01e80e5eb8faa6846ffb5c7e93c6112a3
Tony Reed <tonyreed1542@gmail.com>
2019-09-20 20:01:32 -0500

    remove todos & update formatting

--- public/views/partials/header.php
+++ public/views/partials/header.php
@@ -9,8 +9,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <!-- TODO implement a navbar that looks good to you https://getbootstrap.com/docs/4.3/components/navbar/ -->
-    <!-- TODO give it at least a home page button and a sign in / register button -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">Runescape</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,5 +29,6 @@
             </ul>
         </div>
     </nav>
+    
     <!-- In this div is where all of our dynamic content will be nested -->
-    <div class='main-container container'>
+    <div class='main-container container mt-4'>
