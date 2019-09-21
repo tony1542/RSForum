0be95e810483bc945be6751f95b0f76dd3072368
Tony Reed <tonyreed1542@gmail.com>
2019-09-20 20:07:38 -0500

    remove todos

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-// TODO
-// Look at /public/views/home_page.php & complete todos
-// Look at /public/views/partials/header.php & complete todos
-
 include 'vendor/autoload.php';
 
 view('home_page');
