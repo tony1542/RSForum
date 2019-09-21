cb47de280e19c534b756c7e20cfbcdb30c55b397
Tony Reed <tonyreed1542@gmail.com>
2019-09-21 11:18:06 -0500

    poop

--- public/views/home_page.php
+++ public/views/home_page.php
@@ -1,19 +1,63 @@
-<div class="card">
-    <div class="card-body">
-        <h5 class="card-title">Fremennik Exiles</h5>
-        <h6 class="card-subtitle mb-2 text-muted">From the creators of Fremennik isles...</h6>
-        <p class="card-text">
-            The Fremennik Exiles is an upcoming quest in the Fremennik quest series.
-            First pitched in early 2017, development on the quest was done in Mod Ed and Wolf"s personal project time, with a large chunk of the quest
-            being completed before personal projects were cancelled in mid 2018, halting development of the quest until August 2019.
-        </p>
-        
-        <div class="row">
-            <div class="col-sm">
-                <a href="https://www.reddit.com/r/2007scape/comments/d6g3hc/fremennik_exiles_is_next_week/" class="card-link" target="_blank">Reddit Link</a>
+<div class="row">
+    <div class="card col-sm">
+        <div class="card-body">
+            <h5 class="card-title">Fremennik Exiles</h5>
+            <h6 class="card-subtitle mb-2 text-muted">From the creators of Fremennik isles...</h6>
+            <p class="card-text">
+                The Fremennik Exiles is an upcoming quest in the Fremennik quest series.
+                First pitched in early 2017, development on the quest was done in Mod Ed and Wolf"s personal project time, with a large chunk of the quest
+                being completed before personal projects were cancelled in mid 2018, halting development of the quest until August 2019.
+            </p>
+
+            <div class="row">
+                <div class="col-sm">
+                    <a href="https://www.reddit.com/r/2007scape/comments/d6g3hc/fremennik_exiles_is_next_week/" class="card-link" target="_blank">Reddit Link</a>
+                </div>
+                <div class="col-sm d-flex justify-content-end">
+                    <a href="https://oldschool.runescape.wiki/w/The_Fremennik_Exiles/Quick_guide" class="card-link" target="_blank">	Quick Guide</a>
+                </div>
             </div>
-            <div class="col-sm d-flex justify-content-end">
-                <a href="https://oldschool.runescape.wiki/w/The_Fremennik_Exiles/Quick_guide" class="card-link" target="_blank">	Quick Guide</a>
+        </div>
+    </div>
+
+    <div class="card col-sm">
+        <div class="card-body">
+            <h5 class="card-title">Fremennik Exiles</h5>
+            <h6 class="card-subtitle mb-2 text-muted">From the creators of Fremennik isles...</h6>
+            <p class="card-text">
+                The Fremennik Exiles is an upcoming quest in the Fremennik quest series.
+                First pitched in early 2017, development on the quest was done in Mod Ed and Wolf"s personal project time, with a large chunk of the quest
+                being completed before personal projects were cancelled in mid 2018, halting development of the quest until August 2019.
+            </p>
+
+            <div class="row">
+                <div class="col-sm">
+                    <a href="https://www.reddit.com/r/2007scape/comments/d6g3hc/fremennik_exiles_is_next_week/" class="card-link" target="_blank">Reddit Link</a>
+                </div>
+                <div class="col-sm d-flex justify-content-end">
+                    <a href="https://oldschool.runescape.wiki/w/The_Fremennik_Exiles/Quick_guide" class="card-link" target="_blank">	Quick Guide</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card col-sm">
+        <div class="card-body">
+            <h5 class="card-title">Fremennik Exiles</h5>
+            <h6 class="card-subtitle mb-2 text-muted">From the creators of Fremennik isles...</h6>
+            <p class="card-text">
+                The Fremennik Exiles is an upcoming quest in the Fremennik quest series.
+                First pitched in early 2017, development on the quest was done in Mod Ed and Wolf"s personal project time, with a large chunk of the quest
+                being completed before personal projects were cancelled in mid 2018, halting development of the quest until August 2019.
+            </p>
+
+            <div class="row">
+                <div class="col-sm">
+                    <a href="https://www.reddit.com/r/2007scape/comments/d6g3hc/fremennik_exiles_is_next_week/" class="card-link" target="_blank">Reddit Link</a>
+                </div>
+                <div class="col-sm d-flex justify-content-end">
+                    <a href="https://oldschool.runescape.wiki/w/The_Fremennik_Exiles/Quick_guide" class="card-link" target="_blank">	Quick Guide</a>
+                </div>
             </div>
         </div>
     </div>
