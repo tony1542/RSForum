1fcd5e3dc6704fa0994868c341e4f58d96f10450
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 10:10:08 -0500

    remove old dir (backed up to a new archived repo if you ever need them again)

--- old/Objects/2/object_2_instructions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// 1.  Make your own class somewhat similar to car, don't have it be about cars though
-// 2. Have at least two functions within the function that calculate something & output data
-// (It can be as simple as what I had where you just check a string, or you could check a number)
-// 3. Make sure the object & calling script are separated, use that 'include' statement like i used
