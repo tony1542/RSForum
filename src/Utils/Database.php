68132aeaf9bed4536e1c2e4a9f1b1e5e3d5b3f94
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 10:54:01 -0500

    simplify & add link

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -10,14 +10,12 @@ class Database
      * Create an instance of our database
      *
      * @param bool $debug - If we want more detailed errors
+     *
+     * @see https://www.php.net/manual/en/class.pdo.php
      */
     public function __construct($debug = false)
     {
-        $pdo = new PDO(
-            'mysql:host=localhost;dbname=tonysphpadminabuse',
-            'root',
-            ''
-        );
+        $pdo = new PDO('mysql:host=localhost;dbname=tonysphpadminabuse', 'root', '');
     
         if ($debug) {
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
