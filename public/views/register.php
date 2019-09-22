3b4e42e474a90fef472c2dabe282f05b1c9abfc6
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 10:39:11 -0500

    change ids to be shorter

--- public/views/register.php
+++ public/views/register.php
@@ -4,22 +4,25 @@
             <div class="card-header">Register</div>
             <div class="card-body">
                 <form action="" method="">
-                    <label for="username">Username</label>
-                    <input type="user" class="form-control" id="username" placeholder="Enter Username">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="user" class="form-control" id="username" placeholder="Enter Username">
+                    </div>
+
                     <div class="form-group">
                         <label for="email">Email</label>
                         <input type="email" class="form-control" id="email" placeholder="Enter Email">
                     </div>
+                    
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input type="password" class="form-control" id="password" placeholder="Password">
                     </div>
+                    
                     <div class="form-group">
-                        <!-- Password -->
                         <label for="password_confirm">Password (Confirm)</label>
                         <div class="form-group">
                             <input type="password" class="form-control" id="password_confirm" placeholder="Password">
-                            <p class="help-block">Please confirm password</p>
                         </div>
                         <button type="submit" class="btn btn-primary">Register</button>
                 </form>
