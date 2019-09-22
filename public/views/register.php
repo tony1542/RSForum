67f0af058b64f66eb65bdc5d32666c76e42dc8dc
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 10:39:45 -0500

    match closing tags

--- public/views/register.php
+++ public/views/register.php
@@ -13,19 +13,21 @@
                         <label for="email">Email</label>
                         <input type="email" class="form-control" id="email" placeholder="Enter Email">
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input type="password" class="form-control" id="password" placeholder="Password">
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="password_confirm">Password (Confirm)</label>
                         <div class="form-group">
                             <input type="password" class="form-control" id="password_confirm" placeholder="Password">
                         </div>
                         <button type="submit" class="btn btn-primary">Register</button>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
+</div>
