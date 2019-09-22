452f36f3bc44e8d2b1c81fb702541559143a34b1
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 11:07:10 -0500

    update seed

--- database_seed.sql
+++ database_seed.sql
@@ -5,9 +5,12 @@ CREATE TABLE user
     user_id       int AUTO_INCREMENT,
     username      varchar(12)  NOT NULL,
     email_address varchar(255) NOT NULL,
-    column_4      int          NULL,
     password      varchar(255) NOT NULL,
     CONSTRAINT user_pk
         PRIMARY KEY (user_id)
 );
 
+INSERT INTO user
+SET username      = 'test',
+    email_address = 'test@test.com',
+    password      = 'testpassword';
