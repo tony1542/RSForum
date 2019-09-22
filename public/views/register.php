aa2184fa29cbfac6e997d99e4ae3fc341415387d
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 10:38:02 -0500

    change ids to be shorter

--- public/views/register.php
+++ public/views/register.php
@@ -4,25 +4,25 @@
             <div class="card-header">Register</div>
             <div class="card-body">
                 <form action="" method="">
-                    <label for="exampleInputUser">Username</label>
-                    <input type="user" class="form-control" id="exampleInputUser" aria-describedby="UserHelp" placeholder="Enter Username">
-                <div class="form-group">
-                    <label for="exampleInputEmail">Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="EmailHelp" placeholder="Enter Email">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <!-- Password -->
-                    <label for="password_confirm">Password (Confirm)</label>
+                    <label for="username">Username</label>
+                    <input type="user" class="form-control" id="username" placeholder="Enter Username">
                     <div class="form-group">
-                        <input type="password" class="form-control" id="password_confirm" placeholder="Password">
-                        <p class="help-block">Please confirm password</p>
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Enter Email">
                     </div>
-                <button type="submit" class="btn btn-primary">Register</button>
-            </form>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <!-- Password -->
+                        <label for="password_confirm">Password (Confirm)</label>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password_confirm" placeholder="Password">
+                            <p class="help-block">Please confirm password</p>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Register</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
