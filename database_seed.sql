eec6389ec395728d25281c56c634c87a1ecb8d6a
Tony Reed <tonyreed1542@gmail.com>
2019-09-22 11:02:45 -0500

    update seed

--- database_seed.sql
+++ database_seed.sql
@@ -1,3 +1,13 @@
-CREATE DATABASE IF NOT EXISTS TonysPHPAdminAbuse;
+CREATE DATABASE IF NOT EXISTS tonysphpadminabuse;
 
-# Upcoming will be our users table
+CREATE TABLE user
+(
+    user_id       int AUTO_INCREMENT,
+    username      varchar(12)  NOT NULL,
+    email_address varchar(255) NOT NULL,
+    column_4      int          NULL,
+    password      varchar(255) NOT NULL,
+    CONSTRAINT user_pk
+        PRIMARY KEY (user_id)
+);
+
