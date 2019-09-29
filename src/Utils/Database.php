7837d12fda1ee079d7ff0cb5e499eeb21347dc60
Tony Reed <tonyreed1542@gmail.com>
2019-09-29 08:49:08 -0500

    add debug function

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -3,6 +3,7 @@
 namespace App\Utils;
 
 use PDO;
+use PDOStatement;
 
 class Database
 {
@@ -23,5 +24,22 @@ class Database
         }
     
         return $pdo;
+    }
+    
+    /**
+     * Allows us to see more details about why a query might not have worked like we wanted
+     *
+     * @param PDOStatement $statement
+     *
+     * @see https://www.php.net/manual/en/pdostatement.debugdumpparams.php
+     */
+    public static function debugQuery(PDOStatement $statement)
+    {
+        // TODO explode this on something and get only the 'sent sql:' portion
+        ob_start();
+        $statement->debugDumpParams();
+        $contents = ob_get_clean();
+        
+        dump($contents);
     }
 }
