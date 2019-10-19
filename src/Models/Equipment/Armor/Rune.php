b60b61d58d03feb94ecfd12070d58c0f770e0f9b
Tony Reed <tonyreed1542@gmail.com>
2019-10-19 10:46:53 -0500

    format

--- src/Models/Equipment/Armor/Rune.php
+++ src/Models/Equipment/Armor/Rune.php
@@ -2,8 +2,10 @@
 
 namespace App\Models\Equipment\Armor;
 
-class Rune extends Armor {
-    public function getDefenceLevel() {
+class Rune extends Armor
+{
+    public function getDefenceLevel()
+    {
         return 40;
     }
 }
