f515242a1ce1556f47af865bbd02a497fc55f6fd
Tony Reed <tonyreed1542@gmail.com>
2019-10-28 21:01:22 -0500

    add basic skill class & thumbnail lookup

---
+++ src/Utils/Skills.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Utils;
+
+class Skills
+{
+    const ALL = [
+        'Overall',
+        'Attack',
+        'Defence',
+        'Strength',
+        'Hitpoints',
+        'Ranged',
+        'Prayer',
+        'Magic',
+        'Cooking',
+        'Woodcutting',
+        'Fletching',
+        'Fishing',
+        'Firemaking',
+        'Crafting',
+        'Smithing',
+        'Mining',
+        'Herblore',
+        'Agility',
+        'Thieving',
+        'Slayer',
+        'Farming',
+        'Runecrafting',
+        'Hunter',
+        'Construction'
+    ];
+    
+    // Our path to the skill thumbnails
+    const PATH_TO_ICONS = '/public/Images/OSRS/Skills/';
+    
+    /**
+     * Get skill name out of our array given an index
+     *
+     * @param int $index
+     *
+     * @return string
+     */
+    public static function getSkillNameFromIndex($index) {
+        return self::ALL[$index];
+    }
+    
+    /**
+     * Gets a skill icon thumbnail path from an index
+     *
+     * @param int $index
+     *
+     * @return string - Icon location
+     */
+    public static function getSkillIconFromIndex($index) {
+        $skillName = self::getSkillNameFromIndex($index);
+        $path = self::PATH_TO_ICONS . strtolower($skillName) . '.png';
+        
+        return $path;
+    }
+}
