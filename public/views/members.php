ff2e65fb25ecf3b83e14e5e540e63940edd77813
Tony Reed <tonyreed1542@gmail.com>
2019-12-08 11:31:01 -0600

    add view

---
+++ public/views/members.php
@@ -0,0 +1,11 @@
+<div class="card-columns">
+    <?php /** @var \App\Models\User $member */ ?>
+    <?php foreach ($data['members'] as $member) : ?>
+        <div class="card">
+            <div class="card-header"><?= $member->getUsername() ?></div>
+            <div class="card-body text-center">
+                <img class="img-fluid" src="/public/images/comic2.png" />
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
