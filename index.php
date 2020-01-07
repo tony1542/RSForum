c0c635cfe11085e9ba757ad0e847ecd33f0512e8
tony1542 <tonyreed1542@gmail.com>
2020-01-07 09:48:05 -0600

    EVEN MORE

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ try {
     view('partials/error', [
         'errors' => [
             '<h4>Message</h4> ' . $t->getMessage(),
+            '<h4>File</h4><pre>' . $t->getFile() . '</pre>',
+            '<h4>Line</h4><pre>' . $t->getLine() . '</pre>',
             '<h4>Trace</h4><pre>' . $t->getTraceAsString() . '</pre>'
         ]
     ]);
