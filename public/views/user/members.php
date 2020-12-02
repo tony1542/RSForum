4cb16ffe05b8636c602e19bc445125abb52151c9
Tony Reed <tonyreed1542@gmail.com>
2020-12-01 21:26:27 -0600

    add link to view other's page

--- public/views/user/members.php
+++ public/views/user/members.php
@@ -2,7 +2,14 @@
     <?php /** @var \App\Models\User\User $member */ ?>
     <?php foreach ($data['members'] as $member) : ?>
         <div class="card">
-            <div class="card-header"><?= $member->getUsername() ?></div>
+            <div class="card-header flex">
+                <?= $member->getUsername() ?>
+                <span class="float-right">
+                    <a target="_blank" href="/User/Details/<?= $member->getID() ?>">
+                        <i class="fas fa-eye"></i>
+                    </a>
+                </span>
+            </div>
             <div class="card-footer">
                 Total level: <?= $member->getTotalLevel() ?: 'N/A' ?>
             </div>
