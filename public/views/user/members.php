b985c2547c2570a1d52e17c5ad0bf1415f5d58a8
Tony Reed <tonyreed1542@gmail.com>
2020-12-01 21:31:48 -0600

    use flexbox

--- public/views/user/members.php
+++ public/views/user/members.php
@@ -2,9 +2,9 @@
     <?php /** @var \App\Models\User\User $member */ ?>
     <?php foreach ($data['members'] as $member) : ?>
         <div class="card">
-            <div class="card-header flex">
-                <?= $member->getUsername() ?>
-                <span class="float-right">
+            <div class="card-header d-flex justify-content-between">
+                <span><?= $member->getUsername() ?></span>
+                <span>
                     <a target="_blank" href="/User/Details/<?= $member->getID() ?>">
                         <i class="fas fa-eye"></i>
                     </a>
