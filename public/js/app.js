03f46ab2ce001d378a401eee424eb4a1d5ed1164
Tony <tonyreed1542@gmail.com>
2021-02-11 20:52:41 -0600

    fluff we dont need

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,6 @@ import '../scss/app.scss';
 
 import $ from "jquery";
 
-import EventListener from "./Listeners/eventListener";
-import Nav from "./Listeners/nav";
-
 $(document).ready(function () {
-    EventListener.registerListeners([
-        new Nav()
-    ]);
+
 });
