2ecf19cf6ee4a007f520941bff9646414b2a2c97
Tony <tonyreed1542@gmail.com>
2021-02-14 18:34:57 -0600

    helper class

---
+++ public/js/sweetAlert.js
@@ -0,0 +1,29 @@
+import swal from 'sweetalert';
+
+export default class SweetAlert {
+    static error(title, text) {
+        this.alert(title, text, 'error');
+    }
+
+    static success(title, text) {
+        this.alert(title, text, 'success');
+    }
+
+    static alert(title, text, type) {
+        swal(title, text, type);
+    }
+
+    static confirm(text, callback) {
+        swal({
+            title: "Are you sure?",
+            text: text,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true
+        }).then((if_confirmed) => {
+            if (if_confirmed) {
+                callback();
+            }
+        });
+    }
+}
