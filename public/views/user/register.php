37341be3612ee9eccbc10d473aadbbffa93a3463
Tony <tonyreed1542@gmail.com>
2021-03-02 17:49:28 -0600

    update register form semantics

--- public/views/user/register.php
+++ public/views/user/register.php
@@ -6,17 +6,14 @@
                 <form method="post">
                     <div class="form-group">
                         <label for="username">Username</label>
-
-                        <input name="username" class="form-control" id="username" placeholder="Enter Username">
-
+                        <input name="username" class="form-control" id="username" placeholder="Settled">
                     </div>
-
                     <div class="form-group">
-                        <label for="email">Email</label>
-                        <input name="email_address" class="form-control" id="email_address" placeholder="Enter Email">
+                        <label for="email_address">Email</label>
+                        <input name="email_address" class="form-control" id="email_address" placeholder="not_a_bot@jagex.com">
                     </div>
                    <div class="form-group">
-                       <label for="acc_type">Account Type:</label>
+                       <label for="acc_type">Account Type</label>
                        <select id="acc_type" name="acc_type" class="form-control">
                            <?php foreach (\App\Utils\Runescape\AccountType::getAll() as $account_type_id => $account_type_text) : ?>
                                <option value="<?= $account_type_id ?>"><?= $account_type_text ?></option>
@@ -25,14 +22,13 @@
                    </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type ="password" name="password" class="form-control" id="password" placeholder="Password">
+                        <input type ="password" name="password" class="form-control" id="password">
                     </div>
-
                     <div class="form-group">
                         <label for="password_confirm">Password (Confirm)</label>
-                        <div class="form-group">
-                            <input type ="password" name="password_confirm" class="form-control" id="password_confirm" placeholder="Password">
-                        </div>
+                        <input type ="password" name="password_confirm" class="form-control" id="password_confirm">
+                    </div>
+                    <div>
                         <button class="btn btn-primary">Register</button>
                     </div>
                 </form>
