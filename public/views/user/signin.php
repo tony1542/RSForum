5ab5acd39a4ca9f454e5e8ee3ca4f1b4e54b91cc
Tony <tonyreed1542@gmail.com>
2021-03-02 17:44:28 -0600

    remove unused inputs

--- public/views/user/signin.php
+++ public/views/user/signin.php
@@ -1,43 +1,22 @@
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Sign In</div>
-            <div class="card-body">
-                <form method="post">
-                    <div class="form-group row">
-                        <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                        <div class="col-md-6">
-                            <input type="text" id="email_address" class="form-control" name="email_address" autofocus>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                        <div class="col-md-6">
-                            <input type="password" id="password" class="form-control" name="password" >
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember"> Remember Me
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            Sign In
-                        </button>
-                        <a href="#" class="btn btn-link">
-                            Forgot Your Password?
-                        </a>
-                    </div>
-                </form>
-            </div>
+<div class="row flex justify-center">
+    <div class="card">
+        <div class="card-header">Sign In</div>
+        <div class="card-body">
+            <form method="post">
+                <div class="form-group">
+                    <label for="email_address" class="form-label">Email Address</label>
+                    <input type="text" id="email_address" class="form-control" name="email_address" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" id="password" class="form-control" name="password">
+                </div>
+                <div>
+                    <button class="btn btn-primary">
+                        Sign In
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
