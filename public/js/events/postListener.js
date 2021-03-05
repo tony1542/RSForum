1f1270c4a52c20ab415456e1b33506295a7b5cd4
Tony <tonyreed1542@gmail.com>
2021-03-05 15:26:58 -0600

    add TODO for confirm implementation

---
+++ public/js/events/postListener.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import SweetAlert from '../sweetAlert';
+import Event from '../events/event.js';
+
+export default class PostListener extends Event {
+	register() {
+		// TODO implement a click handler to confirm the delete functionality
+		// TODO use our SweetAlert functionality; look below for an example
+		/** @see ProfileListener.register() */
+	}
+}
