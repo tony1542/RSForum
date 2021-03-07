6bc821d2fea87a967d64f3dcdc8e65eee9703ac2
Tony <tonyreed1542@gmail.com>
2021-03-07 14:25:31 -0600

    remove todo

--- public/js/events/postListener.js
+++ public/js/events/postListener.js
@@ -4,9 +4,6 @@ import Event from '../events/event.js';
 
 export default class PostListener extends Event {
 	register() {
-		// TODO implement a click handler to confirm the delete functionality
-		// TODO use our SweetAlert functionality; look below for an example
-		/** @see ProfileListener.register() */
 		$('.deletePost').click(function (e) {
 			e.preventDefault();
 
