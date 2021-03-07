275b63a951859ae1dfc1985246804b6eaf40a69b
Tony <tonyreed1542@gmail.com>
2021-03-07 13:01:01 -0600

    capture order

--- public/js/Request.js
+++ public/js/Request.js
@@ -1,6 +1,8 @@
 export default class Request {
 	static post(url, params) {
 		params = params || {};
-		$.post('/' + url, params);
+		$.post('/' + url, params).done(function (response) {
+			return response;
+		});
 	}
 }
--- public/js/events/taskListener.js
+++ public/js/events/taskListener.js
@@ -1,6 +1,6 @@
-import $ from 'jquery';
 import SweetAlert from '../sweetAlert';
 import Event from '../events/event.js';
+import Request from "../Request";
 
 export default class TaskListener extends Event {
     register() {
@@ -10,6 +10,17 @@ export default class TaskListener extends Event {
             SweetAlert.confirm('Click ok to delete this task', function () {
                 $('#deleteForm').submit();
             });
+        });
+    
+        $('#sortable').sortable({
+            // On sort release, make request to update DB
+            stop: function (event, ui) {
+                let sorted_card_ids = $('.card-sortable').map(function () {
+                    return $(this).data('id');
+                }).get();
+                
+                Request.post('Task/Sort', {'sorted_card_ids': sorted_card_ids});
+            }
         });
     }
 }
