077c4150aeb089327aa1a31acaa9db8da7209f37
Tony <tonyreed1542@gmail.com>
2021-12-04 16:03:35 -0600

    formatting & add todos

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,47 @@
 <template>
-  <div id="app">
-    <div class="nav">
-      <router-link to="/">Home</router-link>|<router-link to="/about"
-        >About</router-link
-      >
+    <div id="app">
+        <div class="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view />
-  </div>
 </template>
 
+<!-- TODO extract this into a sass file that is included within main.js -->
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
 
 :root {
-  --font: Roboto, sans-serif;
-  --textColor: #374961;
-  --linkActiveColor: #41b783;
+    --font: Roboto, sans-serif;
+    --textColor: #374961;
+    --linkActiveColor: #41b783;
 }
 
 #app {
-  font-family: var(--font);
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: var(--textColor);
+    font-family: var(--font);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: var(--textColor);
 
-  .logo {
-    width: 20%;
-  }
+    .logo {
+        width: 20%;
+    }
 }
 
 .nav {
-  padding: 30px 0 100px 0;
+    padding: 30px 0 100px 0;
 
-  a {
-    font-weight: 500;
-    color: var(--textColor);
-    margin: 0 5px;
-  }
+    a {
+        font-weight: 500;
+        color: var(--textColor);
+        margin: 0 5px;
+    }
 
-  a.router-link-exact-active {
-    color: var(--linkActiveColor);
-  }
+    a.router-link-exact-active {
+        color: var(--linkActiveColor);
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// TODO include sass file in here
+
 new Vue({
   router,
   render: (h) => h(App),
