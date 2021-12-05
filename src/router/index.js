724aa8d75cd45c0fbca4e53039a7cd35e52ffa6a
Tony <tonyreed1542@gmail.com>
2021-12-05 11:30:51 -0600

    WIP forms

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,16 @@ const routes = [
 		name: "About",
 		component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
 	},
+	{
+		path: "/SignIn",
+		name: "Sign In",
+		component: () => import(/* webpackChunkName: "signin" */ "../views/SignIn.vue"),
+	},
+	{
+		path: "/Register",
+		name: "Register",
+		component: () => import(/* webpackChunkName: "register" */ "../views/Register.vue"),
+	},
 ];
 
 const router = new VueRouter({
---
+++ src/views/SignIn.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <Card>
+            <template v-slot:header>
+                Sign in
+            </template>
+
+            <div>
+                <form class="flex flex-col justify-between">
+                    <div>
+                        <label for="username">Username</label>
+                        <input type="text" id="username">
+                    </div>
+                    <div>
+                        <label for="password">Password</label>
+                        <input type="text" id="password">
+                    </div>
+                    <div>
+                        <button class="primary">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </Card>
+    </div>
+</template>
+
+<script>
+    import Card from "./partials/Card";
+    export default {
+        name: "SignIn",
+        components: {
+            Card
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
