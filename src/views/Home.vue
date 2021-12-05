9fa3e8b9c674a71f04a6f285e62dccd55959cbd6
Tony <tonyreed1542@gmail.com>
2021-12-05 11:12:45 -0600

    add card partial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
     <div>
-        <h1>{{ text }}</h1>
+        <Card>
+            <p>
+                This application allows you to connect to a game's (Old School Runescape) high scores and view various information about an account.
+            </p>
+            To accomplish this:
+            <ol>
+                <li>Create an account under the 'register' menu option <em>("Settled" can be a username if you don't know one)</em></li>
+                <li>Click the 'Profile' menu option to see relevant stats</li>
+            </ol>
+
+            <hr>
+
+            To create a home-page post, you must be an admin user. There is a boolean column in the 'user' table that can be updated quick to view that functionality
+
+            <template v-slot:footer>
+                {{ footerText }}
+            </template>
+        </Card>
     </div>
 </template>
 
 <script>
+    import Card from "./partials/Card";
     export default {
         name: 'Home',
+        components: {Card},
         data() {
             return {
-                text: 'Hello world'
+                text: 'Hello world',
+                footerText: 'There are currently no posts. Sign in or create an account and create the first!'
             }
         }
     }
---
+++ src/views/partials/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col justify-between">
+        <header>
+            <slot name="header"></slot>
+            <hr v-show="$slots.header">
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+
+        <footer>
+            <hr v-show="$slots.footer">
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Card"
+    }
+</script>
+
+<style scoped>
+main {
+    padding: 10px 30px 20px 30px;
+    background-color: var(--white);
+    border: 1px solid rgba(42, 51, 58, 0.125);
+    border-radius: .25rem;
+}
+</style>
