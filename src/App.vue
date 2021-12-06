494a924acd28d78a349f452fd8a87e618ac1f6bb
Tony <tonyreed1542@gmail.com>
2021-12-05 19:06:54 -0600

    center loading msg

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
         <!-- Render requested component -->
         <div class="flex justify-center align-items-center">
             <div class="content rounded">
-	            <!-- TODO make this not look so rudimentary; maybe a spinner with our primary color? -->
-	            <div class="loader" v-show="store.waitingOnAjax">
+	            <div class="flex flex-col items-center" v-show="store.waitingOnAjax">
 		            <h2>Loading..</h2>
 		            <LoadingSpinner />
 	            </div>
