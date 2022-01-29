15e35460d32e22b4aac3b23f259379436264bfc6
Tony <tonyreed1542@gmail.com>
2022-01-28 19:54:36 -0600

    save todo

--- server/Models/RSS/RSS.php
+++ server/Models/RSS/RSS.php
@@ -8,6 +8,7 @@ abstract class RSS
 {
     protected SimpleXMLElement $contents;
 
+    // TODO save contents to DB with hash for future retrievals
     public function __construct($url)
     {
         $this->contents = simplexml_load_string(file_get_contents($url));
