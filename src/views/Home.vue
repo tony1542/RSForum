4efdfede0e757df35588bfdc83312febf5b12c45
Tony <tonyreed1542@gmail.com>
2022-01-28 19:44:49 -0600

    save WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,39 @@
 <template>
     <div>
-        <Card>
-            <p>
-                This application allows you to connect to a game's (Old School Runescape) high scores and view various information about an account.
-            </p>
-            To accomplish this:
-            <ol>
-                <li>Create an account under the 'register' menu option <em>("Settled" can be a username if you don't know one)</em></li>
-                <li>Click the 'Profile' menu option to see relevant stats</li>
-            </ol>
+	    <Card v-for="post in feed" v-bind:key="feed.guid" class="mb-8">
+		    <h3>
+			    {{ post.description }}
+		    </h3>
 
-            <hr>
+		    <p>
+			    {{ post.description }}
+		    </p>
 
-            To create a home-page post, you must be an admin user. There is a boolean column in the 'user' table that can be updated quick to view that functionality
+		    <template v-slot:footer>
+			    <a :href="feed.link">
+				    Read more...
+			    </a>
+		    </template>
+	    </Card>
 
-            <template v-slot:footer>
-                {{ footerText }}
-            </template>
-        </Card>
+<!--        <Card>-->
+<!--            <p>-->
+<!--                This application allows you to connect to a game's (Old School Runescape) high scores and view various information about an account.-->
+<!--            </p>-->
+<!--            To accomplish this:-->
+<!--            <ol>-->
+<!--                <li>Create an account under the 'register' menu option <em>("Settled" can be a username if you don't know one)</em></li>-->
+<!--                <li>Click the 'Profile' menu option to see relevant stats</li>-->
+<!--            </ol>-->
+
+<!--            <hr>-->
+
+<!--            To create a home-page post, you must be an admin user. There is a boolean column in the 'user' table that can be updated quick to view that functionality-->
+
+<!--            <template v-slot:footer>-->
+<!--                {{ footerText }}-->
+<!--            </template>-->
+<!--        </Card>-->
     </div>
 </template>
 
@@ -30,14 +46,14 @@
         components: {Card},
         data() {
             return {
-                text: 'Hello world',
-                footerText: 'There are currently no posts. Sign in or create an account and create the first!'
+                footerText: 'There are currently no posts. Sign in or create an account and create the first!',
+	            feed: []
             }
         },
 	    created() {
-		    let request = new Request('AccountType/All');
+		    let request = new Request('RS/Home');
 		    request.call()
-			    .then(data => this.accountTypes = data);
+			    .then(data => this.feed = data.channel.item);
 	    }
     }
 </script>
