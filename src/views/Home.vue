49c0f0b0863b7be25ac89d4cb7c10df7810bffa1
Tony <tonyreed1542@gmail.com>
2022-02-02 21:55:38 -0600

    iterate RSS posts and display them on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,41 @@
 <template>
     <div>
-	    <Card v-for="post in feed" v-bind:key="feed.guid" class="mb-8">
-		    <h3>
-			    {{ post.description }}
-		    </h3>
+        <Card class="mb-8">
+            <p>
+                This application allows you to connect to a game's (Old School Runescape) high scores and view various information about an account.
+            </p>
+            To accomplish this:
+            <ol>
+                <li>Create an account under the 'register' menu option <em>("Settled" can be a username if you don't know one)</em></li>
+                <li>Click the 'Profile' menu option to see relevant stats</li>
+            </ol>
 
-		    <p>
-			    {{ post.description }}
-		    </p>
+            <template v-slot:footer>
+                {{ footerText }}
+            </template>
+        </Card>
+
+	    <Card v-for="post in feed" v-bind:key="feed.guid" class="mb-8">
+			<template v-slot:header>
+				{{ post.title }}
+			</template>
+
+		    <div class="flex align-items-center">
+			    <div class="pr-6">
+				    <img :src="post.enclosure['@attributes'].url" alt="Descriptive Image" />
+			    </div>
+
+			    <div>
+				    {{ post.description }}
+			    </div>
+		    </div>
 
 		    <template v-slot:footer>
-			    <a :href="feed.link">
-				    Read more...
+			    <a :href="post.link" target="_blank">
+				    Read more..
 			    </a>
 		    </template>
 	    </Card>
-
-<!--        <Card>-->
-<!--            <p>-->
-<!--                This application allows you to connect to a game's (Old School Runescape) high scores and view various information about an account.-->
-<!--            </p>-->
-<!--            To accomplish this:-->
-<!--            <ol>-->
-<!--                <li>Create an account under the 'register' menu option <em>("Settled" can be a username if you don't know one)</em></li>-->
-<!--                <li>Click the 'Profile' menu option to see relevant stats</li>-->
-<!--            </ol>-->
-
-<!--            <hr>-->
-
-<!--            To create a home-page post, you must be an admin user. There is a boolean column in the 'user' table that can be updated quick to view that functionality-->
-
-<!--            <template v-slot:footer>-->
-<!--                {{ footerText }}-->
-<!--            </template>-->
-<!--        </Card>-->
     </div>
 </template>
 
