c6c13a8e47183d1b632fb2827a323bfb350c9dec
Tony <tonyreed1542@protonmail.com>
2022-06-26 12:42:25 -0500

    wip - look into splitting into two projects

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,28 @@
 FROM php:8.1-apache
 WORKDIR /var/www/html
 
+# Copy files over and expose port
+COPY server/ server
+COPY composer.json ./
+
+# TODO somehow to init the db said against our db container?
+
 # Install git, PDO, mysqli and enable apache rewriting
 RUN apt-get update \
-    && apt-get install -y git
-RUN docker-php-ext-install pdo pdo_mysql mysqli
+    && apt-get install -y git \
+    libzip-dev \
+    zip
+RUN docker-php-ext-install pdo pdo_mysql mysqli zip
 RUN a2enmod rewrite
 
 # Install utils
 RUN apt-get install -y nano
 
-# Install Composer
+# Install Composer & run the install for project dependencies
 RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 RUN php composer-setup.php --install-dir=. --filename=composer
 RUN mv composer /usr/local/bin/
+RUN composer install
 
-# Copy files over and expose port
-COPY server/ server
+# Expose the port to use
 EXPOSE 80
--- server/index.php
+++ server/index.php
@@ -1,5 +1,56 @@
 <?php
 
-echo phpinfo();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: POST, GET, PATCH, DELETE');
+header('Content-Type: application/json');
+
+require('vendor/autoload.php');
+
+use App\Models\User\User;
+use App\Utils\EnvException;
+use App\Utils\Http\JWTAuthenticator;
+use App\Utils\Http\Router;
+use App\Utils\Http\Server;
+
+try {
+    setApplicationVariables();
+    
+    $header = Server::getAuthHeader();
+    if ($header) {
+        $decoded = JWTAuthenticator::authenticate($header);
+        
+        // JWT failed
+        if ($decoded === false) {
+            header('HTTP/1.1 401 Unauthorized');
+            jsonResponse([
+                'message' => 'Access denied.'
+            ]);
+        } else {
+            setSignedInUser(new User($decoded->data->id));
+        }
+    }
+    
+    Router::callAction();
+} catch (Throwable $t) {
+    $errors = ['Something went wrong.'];
+    
+    if (Server::isLocalHost() || getSignedInUser()->isAdmin()) {
+        $errors = [
+            'Message ' . $t->getMessage(),
+            'File '    . $t->getFile(),
+            'Line '    . $t->getLine(),
+            'Trace '   . $t->getTraceAsString(),
+        ];
+        
+        if ($t instanceof EnvException) {
+            $errors = [$t->getMessage()];
+        }
+    }
+    
+    \jsonResponse([
+        'errors' => $errors
+    ]);
+}
 
 die;
