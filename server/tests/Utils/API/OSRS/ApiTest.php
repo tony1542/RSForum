5197fe0fd4ffe15302786917052b4bf5c5a5a76e
tony1542 <tonyreed1542@protonmail.com>
2025-02-15 21:37:45 -0600

    add coverage for other acct types

--- server/tests/Utils/API/OSRS/ApiTest.php
+++ server/tests/Utils/API/OSRS/ApiTest.php
@@ -3,19 +3,24 @@
 namespace Tests\Utils\API\OSRS;
 
 use App\Utils\API\OSRS\Api;
+use App\Utils\Runescape\AccountType;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
 {
     public function testGetStatsForPlayer(): void
     {
-        $this->assertNotEmpty(Api::getStatsForPlayer('Lynx Titan', 0));
-        $this->assertTrue(true);
+        $this->assertNotEmpty(Api::getStatsForPlayer('Lynx Titan', AccountType::PLAYER_TYPE_NORMAL));
+        $this->assertNotEmpty(Api::getStatsForPlayer('Iron Hyger', AccountType::PLAYER_TYPE_IRONMAN));
+        $this->assertNotEmpty(Api::getStatsForPlayer('groonoomcroo', AccountType::PLAYER_TYPE_HARDCORE_IRONMAN));
+        $this->assertNotEmpty(Api::getStatsForPlayer('Settled', AccountType::PLAYER_TYPE_ULTIMATE_IRONMAN));
     }
 
     public function testGetAccoladesForPlayer(): void
     {
-        $this->assertNotEmpty(Api::getAccoladesForPlayer('Lynx Titan', 0));
-        $this->assertTrue(true);
+        $this->assertNotEmpty(Api::getAccoladesForPlayer('Lynx Titan', AccountType::PLAYER_TYPE_NORMAL));
+        $this->assertNotEmpty(Api::getAccoladesForPlayer('Iron Hyger', AccountType::PLAYER_TYPE_IRONMAN));
+        $this->assertNotEmpty(Api::getAccoladesForPlayer('groonoomcroo', AccountType::PLAYER_TYPE_HARDCORE_IRONMAN));
+        $this->assertNotEmpty(Api::getAccoladesForPlayer('Settled', AccountType::PLAYER_TYPE_ULTIMATE_IRONMAN));
     }
 }
